d1a07d79ded781fba636062d406caeaf18d79333
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-08 16:24:33 -0300

    Adiciona classe RebelDto.

---
+++ src/main/java/br/com/letscode/api/starwars/dtos/RebelDto.java
@@ -0,0 +1,18 @@
+package br.com.letscode.api.starwars.dtos;
+
+import br.com.letscode.api.starwars.enums.GenderEnum;
+import br.com.letscode.api.starwars.models.LocationModel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+@Getter
+public class RebelDto {
+
+    private String  name;
+    private Integer age;
+    private GenderEnum gender;
+    private LocationModel location;
+
+}
