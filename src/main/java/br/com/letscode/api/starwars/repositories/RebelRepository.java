2436dc638f5421bf68c52197ee71d4ddcc202749
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-08 19:33:32 -0300

    Adiciona método updateRebelLocation

--- src/main/java/br/com/letscode/api/starwars/repositories/RebelRepository.java
+++ src/main/java/br/com/letscode/api/starwars/repositories/RebelRepository.java
@@ -1,55 +1,53 @@
 package br.com.letscode.api.starwars.repositories;
 
-import br.com.letscode.api.starwars.dtos.RebelReturnDto;
+import br.com.letscode.api.starwars.dtos.CurrentLocationDto;
 import br.com.letscode.api.starwars.enums.GenderEnum;
-import br.com.letscode.api.starwars.models.Location;
-import br.com.letscode.api.starwars.models.Rebel;
+import br.com.letscode.api.starwars.models.LocationModel;
+import br.com.letscode.api.starwars.models.RebelModel;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public class RebelRepository {
 
-    private static List<Rebel> rebels = new ArrayList<>();
+    private static List<RebelModel> rebels = new ArrayList<>();
 
     static {
 
-        rebels.addAll(Arrays.asList(new Rebel("Chuew",
+        rebels.addAll(Arrays.asList(new RebelModel("Chuew",
                 45,
                 GenderEnum.MALE,
-                new Location(-50, 45, "bEt")
-        ), new Rebel("Chue2",
+                new LocationModel(-50, 45, "bEt")
+        ), new RebelModel("Chue2",
                 45,
                 GenderEnum.MALE,
-                new Location(-50, 45, "bEt")
+                new LocationModel(-50, 45, "bEt")
         )));
     }
 
-    public Rebel save(Rebel rebel){
-        rebel.setId(UUID.randomUUID());
+    public RebelModel cadastrar(RebelModel rebel){
+        System.out.println("repository cadastrar");
+        rebel.setId((long) (rebels.size() + 1));
         rebels.add(rebel);
+        System.out.println(rebels.size());
         return rebel;
     }
 
-    public List<Rebel> getAll() {
+    public List<RebelModel> getAll() {
         return rebels;
     }
 
-    public Rebel findById(Long id){
+    public RebelModel findById(Long id){
         System.out.println("repository findById");
         System.out.println("id: " + id);
-        Rebel returnedRebel = rebels.stream()
+        RebelModel returnedRebel = rebels.stream()
                 .filter(rebel -> rebel.getId().equals(id))
                 .findFirst().get();
         System.out.println(returnedRebel);
         return returnedRebel;
     }
-
-//    public Object save(RebelDto rebel) {
-//    }
 }
 
