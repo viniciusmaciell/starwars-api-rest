4a915b2411afc1ca648d7bdfe8026ea3735a4bf0
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-08 19:34:41 -0300

    Muda setCurrentLocation para setRebelCurrentLocation e retorna um ReturnedRebelDto

--- src/main/java/br/com/letscode/api/starwars/services/RebelService.java
+++ src/main/java/br/com/letscode/api/starwars/services/RebelService.java
@@ -2,9 +2,8 @@ package br.com.letscode.api.starwars.services;
 
 import br.com.letscode.api.starwars.dtos.CurrentLocationDto;
 import br.com.letscode.api.starwars.dtos.RebelDto;
-import br.com.letscode.api.starwars.dtos.RebelReturnDto;
-import br.com.letscode.api.starwars.models.Location;
-import br.com.letscode.api.starwars.models.Rebel;
+import br.com.letscode.api.starwars.models.LocationModel;
+import br.com.letscode.api.starwars.models.RebelModel;
 import br.com.letscode.api.starwars.repositories.RebelRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -20,33 +19,26 @@ public class RebelService {
         this.repository = repository;
     }
 
-    public List<Rebel> getAll() {
+    public List<RebelModel> getAll() {
         return repository.getAll();
 
     }
-//
-//    public Rebel cadastrar(Rebel rebel) {
-//        repository.cadastrar(rebel);
-//        return rebel;
-//    }
+
+    public RebelModel cadastrar(RebelModel rebel) {
+        repository.cadastrar(rebel);
+        return rebel;
+    }
 
     public RebelDto setCurrentLocation(Long id, CurrentLocationDto currentLocationDto) {
         System.out.println("service setCurrentLocation");
-        Rebel rebel = repository.findById(id);
+        RebelModel rebel = repository.findById(id);
         RebelDto rebelDto = new RebelDto();
-        Location location = new Location(currentLocationDto.getLatitude(),
+        LocationModel location = new LocationModel(currentLocationDto.getLatitude(),
                 currentLocationDto.getLongitude(), currentLocationDto.getBaseName());
         rebel.setLocation(location);
-        repository.save(rebel);
+        repository.cadastrar(rebel);
         System.out.println("service - rebel: " + rebel);
         BeanUtils.copyProperties(rebel, rebelDto);
         return rebelDto;
     }
-
-    public Rebel save(RebelDto rebel) {
-        Rebel rebelEntity = new Rebel();
-        BeanUtils.copyProperties(rebel, rebelEntity);
-        return repository.save(rebelEntity);
-    }
 }
-
