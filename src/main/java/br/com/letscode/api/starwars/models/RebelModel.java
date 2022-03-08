577eae978a687668e8745c99be6ef1c7f959339a
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-08 16:25:32 -0300

    Adiciona atributos id e complaints em RebelModel.

--- src/main/java/br/com/letscode/api/starwars/models/RebelModel.java
+++ src/main/java/br/com/letscode/api/starwars/models/RebelModel.java
@@ -3,14 +3,18 @@ package br.com.letscode.api.starwars.models;
 import br.com.letscode.api.starwars.enums.GenderEnum;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class RebelModel {
 
+    private Long id;
     private String  name;
     private Integer age;
 //    @JsonProperty("gender")
     private GenderEnum gender;
     private LocationModel location;
+    private List<Long> complaints;
 
     public RebelModel(String name, Integer age, GenderEnum gender, LocationModel location) {
         this.name = name;
