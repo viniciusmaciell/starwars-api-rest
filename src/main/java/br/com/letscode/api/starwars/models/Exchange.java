061f8b94ae9d3aca8edc8cd899a3e7da713d9b7f
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-10 16:39:31 -0300

    Adiciona construtor para teste

--- src/main/java/br/com/letscode/api/starwars/models/Exchange.java
+++ src/main/java/br/com/letscode/api/starwars/models/Exchange.java
@@ -1,16 +1,20 @@
 package br.com.letscode.api.starwars.models;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class Exchange {
 
     private Long id;
     private List<Item> offer;
     private List<Item> demand;
 
+    public Exchange(long l, List<Item> asList, List<Item> asList1) {
+    }
 }
--- src/main/java/br/com/letscode/api/starwars/models/Item.java
+++ src/main/java/br/com/letscode/api/starwars/models/Item.java
@@ -13,4 +13,6 @@ public class Item {
     private String name;
     private Integer score;
 
+    public Item(long l, String name, Integer score) {
+    }
 }
