7eec5df8da3cf8d75bc2f6bc1fdeb18bef823a5b
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-11 13:51:40 -0300

    Cria makeADeal a partir de exchangeItems. Torna updateInventory private

--- src/main/java/br/com/letscode/api/starwars/services/RebelService.java
+++ src/main/java/br/com/letscode/api/starwars/services/RebelService.java
@@ -1,7 +1,8 @@
 package br.com.letscode.api.starwars.services;
 
 import br.com.letscode.api.starwars.dtos.*;
-import br.com.letscode.api.starwars.models.ProposedExchange;
+import br.com.letscode.api.starwars.models.Deal;
+import br.com.letscode.api.starwars.models.Item;
 import br.com.letscode.api.starwars.models.Location;
 import br.com.letscode.api.starwars.models.Rebel;
 import br.com.letscode.api.starwars.repositories.RebelRepository;
@@ -45,14 +46,6 @@ public class RebelService {
         Rebel rebel = repository.updateRebelLocation(id, location);
         BeanUtils.copyProperties(rebel, returnedRebelDto);
 
-
-//        returnedRebelDto.setId(rebel.getId());
-//        returnedRebelDto.setName(rebel.getName());
-//        returnedRebelDto.setAge(rebel.getAge());
-//        returnedRebelDto.setGender(rebel.getGender());
-//        returnedRebelDto.setLocation(rebel.getLocation());
-
-
         return returnedRebelDto;
     }
 
@@ -61,15 +54,48 @@ public class RebelService {
 
     }
 
-    public List<ProposedExchange> getAllOpenOffers() {
+    public List<Deal> getAllOpenOffers() {
         return repository.getAllOpenOffers();
     }
 
-    public ProposedExchangeDto addOffer(ProposedExchangeDto exchangeDto) {
-        ProposedExchange newOffer = new ProposedExchange();
-        BeanUtils.copyProperties(exchangeDto, newOffer);
-        ProposedExchange savedOffer = repository.addOffer(newOffer);
-        BeanUtils.copyProperties(savedOffer, exchangeDto);
-        return exchangeDto;
+    public ReturnDealDto addOffer(DealDto dealDto) {
+        Deal newOffer = new Deal();
+//        BeanUtils.copyProperties(dealDto, newOffer);
+        newOffer.setPartyId(dealDto.getPartyId());
+        newOffer.setOffer(dealDto.getOffer());
+        newOffer.setDemand(dealDto.getDemand());
+        Deal savedOffer = repository.addOffer(newOffer);
+        ReturnDealDto returnDealDto = new ReturnDealDto();
+        BeanUtils.copyProperties(savedOffer, returnDealDto);
+        return returnDealDto;
+    }
+
+    public Rebel makeADeal(CounterpartyDto counterpartyDto) {
+        Deal deal = repository.getDealById(counterpartyDto.getDealId());
+        Rebel party = repository.findById(deal.getPartyId());
+        Rebel counterparty = repository.findById(counterpartyDto.getCounterpartyId());
+
+        exchangeItems(deal, party, counterparty);
+
+        counterparty = repository.updateInventory(counterparty);
+        repository.updateInventory(party);
+        repository.removeDeal(deal);
+
+        return counterparty;
+    }
+
+    private void exchangeItems(Deal deal, Rebel party, Rebel counterparty) {
+        List<Item> offeredByParty = deal.getOffer();
+        List<Item> demandedByParty = deal.getDemand();
+        updateInventory(party, offeredByParty, demandedByParty);
+        updateInventory(counterparty, demandedByParty, offeredByParty);
+    }
+
+    private Rebel updateInventory(Rebel rebel, List<Item> itemsToRemove, List<Item> itemsToAdd) {
+        rebel.getInventory().addAll(itemsToAdd);
+        for (Item item : itemsToRemove) {
+            rebel.getInventory().remove(item);
+        }
+        return rebel;
     }
 }
