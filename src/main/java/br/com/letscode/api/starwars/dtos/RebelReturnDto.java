5122eb191cac0c862a2e2042981f23e8d0ac69cc
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-11 12:37:42 -0300

    Adiciona atributo inventory

--- src/main/java/br/com/letscode/api/starwars/dtos/RebelReturnDto.java
+++ src/main/java/br/com/letscode/api/starwars/dtos/RebelReturnDto.java
@@ -1,10 +1,12 @@
 package br.com.letscode.api.starwars.dtos;
 
 import br.com.letscode.api.starwars.enums.GenderEnum;
+import br.com.letscode.api.starwars.models.Item;
 import br.com.letscode.api.starwars.models.Location;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -18,4 +20,6 @@ public class RebelReturnDto {
     private GenderEnum gender;
     private Location location;
     private Integer confidenceLevel;
+    private List<Item> inventory;
+
 }
