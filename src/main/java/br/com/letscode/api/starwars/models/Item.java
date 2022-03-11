f09ba44e649336c2dc6089994d1c62a779b78811
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-11 12:40:22 -0300

    Remove construtor

--- src/main/java/br/com/letscode/api/starwars/models/Item.java
+++ src/main/java/br/com/letscode/api/starwars/models/Item.java
@@ -2,23 +2,16 @@ package br.com.letscode.api.starwars.models;
 
 import br.com.letscode.api.starwars.enums.ItemEnum;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.UUID;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
 public class Item {
 
     private UUID id;
     private ItemEnum name;
     private Integer score;
 
-    public Item(UUID id, ItemEnum name, Integer score) {
-        this.id = id;
-        this.name = name;
-        this.score = score;
-    }
 }
