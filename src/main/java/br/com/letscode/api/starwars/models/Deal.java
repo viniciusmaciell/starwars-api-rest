5e04760f90de84c225a4cc138153b2241af5fb14
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-12 11:44:17 -0300

    Adiciona método pointsMatch na classe Deal.

--- src/main/java/br/com/letscode/api/starwars/models/Deal.java
+++ src/main/java/br/com/letscode/api/starwars/models/Deal.java
@@ -3,6 +3,8 @@ package br.com.letscode.api.starwars.models;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,4 +19,17 @@ public class Deal {
     private List<Item> offer;
     private List<Item> demand;
 
+    public void pointsMatch(){
+        Integer pointsOffer = 0;
+        Integer pointsDemand = 0;
+        for(Item item : offer){
+            pointsOffer = item.getScore();
+        }
+        for(Item item : demand){
+            pointsDemand = item.getScore();
+        }
+        if(pointsOffer != pointsDemand){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Supply and demand must have the same score.");
+        }
+    }
 }
