fffa662c45453c93c501b1473426e3f6d93aa6d6
Vinícius Maciel <v.s.maciel@outlook.com>
2022-03-13 12:38:29 -0300

    Adiciona validação no ReportDto

--- src/main/java/br/com/letscode/api/starwars/dtos/ReportDto.java
+++ src/main/java/br/com/letscode/api/starwars/dtos/ReportDto.java
@@ -3,13 +3,15 @@ package br.com.letscode.api.starwars.dtos;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
 @RequiredArgsConstructor
 //@NoArgsConstructor
 public class ReportDto {
-
+    @NotNull
     private UUID rebelId;
+    @NotNull
     private UUID traitorId;
 }
