b5fa5f1ae29c858792699719235b921c70f246f1
Vinícius Maciel <v.s.maciel@outlook.com>
2022-03-13 12:23:51 -0300

    Adiciona validação de campos no RebelDto e Location

--- src/main/java/br/com/letscode/api/starwars/controllers/RebelController.java
+++ src/main/java/br/com/letscode/api/starwars/controllers/RebelController.java
@@ -4,10 +4,12 @@ import br.com.letscode.api.starwars.dtos.*;
 import br.com.letscode.api.starwars.models.Deal;
 import br.com.letscode.api.starwars.models.Rebel;
 import br.com.letscode.api.starwars.services.RebelService;
+import br.com.letscode.api.starwars.utils.ReportMessageEnum;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,14 +24,15 @@ public class RebelController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> saveRebel(@RequestBody RebelDto rebelDto) {
+    public ResponseEntity<Object> saveRebel( @Valid @RequestBody RebelDto rebelDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.saveRebel(rebelDto));
     }
 
     @PostMapping("/report")
     public ResponseEntity<String> reportRebel(@RequestBody ReportDto reportDto) {
         if (!service.isARebel(reportDto.getRebelId())){
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("You are not a validated rebel.");
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+                    .body(ReportMessageEnum.NOT_VALIDATED.toString());
         }
         if (service.isAlreadyTraitor(reportDto.getTraitorId())) {
             return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("This rebel is already a traitor.");
--- src/main/java/br/com/letscode/api/starwars/dtos/RebelDto.java
+++ src/main/java/br/com/letscode/api/starwars/dtos/RebelDto.java
@@ -1,21 +1,29 @@
 package br.com.letscode.api.starwars.dtos;
 
-import br.com.letscode.api.starwars.utils.GenderEnum;
 import br.com.letscode.api.starwars.models.Item;
 import br.com.letscode.api.starwars.models.Location;
+import br.com.letscode.api.starwars.utils.GenderEnum;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 
 @RequiredArgsConstructor
 @Getter
 public class RebelDto {
-
+    @NotBlank
     private String name;
+    @NotNull
     private Integer age;
+    @NotNull
     private GenderEnum gender;
+    @NotNull
+    @Valid
     private Location location;
+
     private List<Item> inventory;
 }
