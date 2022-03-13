d91c44f52a89f99143c6c36f8024979982289802
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-13 20:05:53 -0300

    Adiciona validações NotNull e NotBlank em CurrentLocationDto.java

--- src/main/java/br/com/letscode/api/starwars/dtos/CurrentLocationDto.java
+++ src/main/java/br/com/letscode/api/starwars/dtos/CurrentLocationDto.java
@@ -2,11 +2,17 @@ package br.com.letscode.api.starwars.dtos;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CurrentLocationDto {
 
+    @NotNull
     private Integer latitude;
+    @NotNull
     private Integer longitude;
+    @NotBlank
     private String baseName;
 
 }
