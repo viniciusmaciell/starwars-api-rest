8ac95ebf1ccafc89f0a1af98b428bbe7999dd82e
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-13 20:08:13 -0300

    Adiciona atributo value e getValue() em ItemEnum.java

--- src/main/java/br/com/letscode/api/starwars/utils/ItemEnum.java
+++ src/main/java/br/com/letscode/api/starwars/utils/ItemEnum.java
@@ -1,5 +1,15 @@
 package br.com.letscode.api.starwars.utils;
 
 public enum ItemEnum {
-    WEAPON, AMMUNITION, WATER, FOOD
+    WEAPON(4), AMMUNITION(3), WATER(2), FOOD(1);
+
+    private final int value;
+
+    ItemEnum(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
 }
