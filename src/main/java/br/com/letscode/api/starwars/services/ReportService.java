9c47ffb2715a44aeca704615db24a1b45209b2df
Vinícius Maciel <v.s.maciel@outlook.com>
2022-03-15 22:20:13 -0300

    Adiciona método getAllLostPointsFromTraitors e refatoração

--- src/main/java/br/com/letscode/api/starwars/services/ReportService.java
+++ src/main/java/br/com/letscode/api/starwars/services/ReportService.java
@@ -1,9 +1,11 @@
 package br.com.letscode.api.starwars.services;
 
 import br.com.letscode.api.starwars.dtos.ItemsAmountDto;
+import br.com.letscode.api.starwars.dtos.LostPointsDto;
 import br.com.letscode.api.starwars.models.Rebel;
 import br.com.letscode.api.starwars.repositories.RebelRepository;
 import br.com.letscode.api.starwars.utils.ItemEnum;
+import br.com.letscode.api.starwars.utils.RebelCategoryEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,52 +16,44 @@ public class ReportService {
     private final RebelRepository rebelRepository;
 
     public String getPercentageOfTrustedRebels() {
-        int rebels = rebelRepository.getAll().size();
-        int traitorousRebels = rebelRepository.getAllTraitors().size();
-        float total;
-        String response;
 
-        total = ((float) rebels / (rebels + traitorousRebels)) * 100;
+        Float total = getAmountOfAllRebels(RebelCategoryEnum.RELIABLE_REBELS);
+        if (total.isNaN())
+            return "It was not found any register of rebels.";
 
-        response = "The percentage of trusted rebels is " + total + " %";
-        return response;
+        return "The percentage of trusted rebels is " + total;
     }
 
     public String getPercentageOfTraitors() {
-        int rebels = rebelRepository.getAll().size();
-        int traitorousRebels = rebelRepository.getAllTraitors().size();
-        float total;
-        String response;
+        Float total = getAmountOfAllRebels(RebelCategoryEnum.TRAITORS_REBELS);
+        if (total.isNaN())
+            return "It was not found any register of traitors.";
 
-        total = ((float) traitorousRebels / (rebels + traitorousRebels)) * 100;
-        if (traitorousRebels == 0) {
-            response = "There is not registers of traitors yet.";
-        }
-        response = "The percentage of traitors is " + total + " %";
-        return response;
+        return "The percentage of traitors is " + total;
     }
 
     public ItemsAmountDto getPercentageOfItemsPerRebel() {
-        float waterAmount = 0f;
-        float ammunitionAmount = 0f;
-        float weaponAmount = 0f;
-        float foodAmount = 0f;
+        float waterAmount = 0;
+        float ammunitionAmount = 0;
+        float weaponAmount = 0;
+        float foodAmount = 0;
 
         var rebels = rebelRepository.getAll();
         int numberOfRebels = rebels.size();
 
-        for (Rebel rebel : rebels) {
-            var inventory = rebel.getInventory();
-            for (ItemEnum item : inventory) {
-                if (item.equals(ItemEnum.WATER))
-                    waterAmount++;
-                if (item.equals(ItemEnum.WEAPON)) {
-                    weaponAmount++;
+        if (!rebels.isEmpty()) {
+            for (Rebel rebel : rebels) {
+                var inventory = rebel.getInventory();
+                for (ItemEnum item : inventory) {
+                    if (item.equals(ItemEnum.WATER))
+                        waterAmount++;
+                    if (item.equals(ItemEnum.WEAPON))
+                        weaponAmount++;
+                    if (item.equals(ItemEnum.AMMUNITION))
+                        ammunitionAmount++;
+                    if (item.equals(ItemEnum.FOOD))
+                        foodAmount++;
                 }
-                if (item.equals(ItemEnum.AMMUNITION))
-                    ammunitionAmount++;
-                if (item.equals(ItemEnum.FOOD))
-                    foodAmount++;
             }
         }
         return new ItemsAmountDto(
@@ -68,6 +62,56 @@ public class ReportService {
                 waterAmount / numberOfRebels,
                 foodAmount / numberOfRebels);
 
+    }
+
+    private Float getAmountOfAllRebels(RebelCategoryEnum rebelCategory) {
+        int rebels = rebelRepository.getAll().size();
+        int traitorsRebels = rebelRepository.getAllTraitors().size();
+        float total = 0f;
+
+
+        if (rebelCategory.equals(RebelCategoryEnum.RELIABLE_REBELS)) {
+            total = ((float) rebels / (rebels + traitorsRebels)) * 100;
+        }
+        if (rebelCategory.equals(RebelCategoryEnum.TRAITORS_REBELS)) {
+            total = ((float) traitorsRebels / (rebels + traitorsRebels)) * 100;
+        }
+        return total;
+    }
+
+    public LostPointsDto getAllLostPointsFromTraitors() {
+        int water = 0;
+        int ammunition = 0;
+        int weapon = 0;
+        int food = 0;
+        int totalLostPoints = 0;
+
+        var traitors = rebelRepository.getAllTraitors();
+
+        for (Rebel traitor : traitors) {
+            var inventory = traitor.getInventory();
+            for (ItemEnum lostItem : inventory) {
+
+                if (lostItem.equals(ItemEnum.WATER)) {
+                    water -= ItemEnum.WATER.getValue();
+                    totalLostPoints += ItemEnum.WATER.getValue();
+
+                } else if (lostItem.equals(ItemEnum.WEAPON)) {
+                    weapon -= ItemEnum.WEAPON.getValue();
+                    totalLostPoints += ItemEnum.WEAPON.getValue();
+
+                } else if (lostItem.equals(ItemEnum.AMMUNITION)) {
+                    ammunition -= ItemEnum.AMMUNITION.getValue();
+                    totalLostPoints += ItemEnum.AMMUNITION.getValue();
+
+                } else if (lostItem.equals(ItemEnum.FOOD)) {
+                    food -= ItemEnum.FOOD.getValue();
+                    totalLostPoints += ItemEnum.FOOD.getValue();
+                }
+
+            }
+        }
+        return new LostPointsDto(weapon, ammunition, water, food, totalLostPoints);
     }
 
 }
